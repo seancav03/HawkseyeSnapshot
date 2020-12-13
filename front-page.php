74718c147c1676184cc7f3dab415d7efc938eb4c
Sean Cavalieri <seanc2021@headroyce.org>
2020-12-13 02:23:12 +0000

    Cleared front page. Will start populating soon

--- front-page.php
+++ front-page.php
@@ -1,16 +1,7 @@
 <?php get_header();?>
 
 <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="news-post-wrapper">
-            <div class="news-post-wrapper-sm ">
-                <?php if (have_posts()) : while(have_posts()) : the_post();?>
-                    <?php the_content();?>
-                <?php endwhile; endif;?>
-            </div>
-        </div>
-    </div>
+    
 </div>
 
 
